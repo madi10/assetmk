244cdf5eb0861ba916db5a5766690fce2118f3db
Rahmadi <support@mantankode.net>
2023-09-24 21:09:48 +0800

    ehhh
    ehhh

--- JS/ademk.js
+++ JS/ademk.js
@@ -1,8 +1,9 @@
-<script async='async' data-loading='lazy' >
+<script async='async' data-loading='lazy'>
 var images = [
   {src: "https://images.tokopedia.net/img/cache/700/VqbcmM/2022/10/3/dd2dbcdb-a504-48b6-bc93-c06f4966dfcb.png.webp", href: "https://tokopedia.link/BDrke6PlOCb"},
   {src: "https://images.tokopedia.net/img/cache/700/VqbcmM/2022/6/2/00f390cd-e16d-4cef-989a-7830afd5e69f.png.webp", href: "https://tokopedia.link/Tr3KkjLlOCb"},
   {src: "https://images.tokopedia.net/img/cache/700/VqbcmM/2023/4/3/71c5b53f-66b6-4b8d-b00a-beb887d5cc99.jpg.webp", href: "https://tokopedia.link/0Rldjl7MiDb"},
+  {src: "https://images.tokopedia.net/img/cache/700/product-1/2018/12/21/27311837/27311837_9a0daacb-277b-4a7d-a7cf-baa87175e72a_2048_2048.jpg.webp", href: "https://tokopedia.link/bmOa1nGN0xb"},
   {src: "https://images.tokopedia.net/img/cache/700/VqbcmM/2023/9/13/632c3394-4262-4781-bcf1-998e356013b6.jpg.webp", href: "https://tokopedia.link/1VSR8P2wEzb"}
 ];
   function displayImage() {
