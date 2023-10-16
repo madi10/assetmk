ef4cde55a93858dbeb52780c0439569150a9b550
Rahmadi <support@mantankode.net>
2023-10-17 02:24:32 +0800

    Update ademk.js

--- JS/ademk.js
+++ JS/ademk.js
@@ -1,4 +1,4 @@
-var images = [
+var ademk = [
   {src: "https://images.tokopedia.net/img/cache/700/VqbcmM/2022/10/3/dd2dbcdb-a504-48b6-bc93-c06f4966dfcb.png.webp", href: "https://tokopedia.link/BDrke6PlOCb"},
   {src: "https://images.tokopedia.net/img/cache/700/VqbcmM/2022/6/2/00f390cd-e16d-4cef-989a-7830afd5e69f.png.webp", href: "https://tokopedia.link/Tr3KkjLlOCb"},
   {src: "https://images.tokopedia.net/img/cache/700/VqbcmM/2023/4/3/71c5b53f-66b6-4b8d-b00a-beb887d5cc99.jpg.webp", href: "https://tokopedia.link/0Rldjl7MiDb"},
@@ -6,18 +6,3 @@ var images = [
   {src: "https://images.tokopedia.net/img/cache/700/VqbcmM/2023/9/13/632c3394-4262-4781-bcf1-998e356013b6.jpg.webp", href: "https://tokopedia.link/1VSR8P2wEzb"},
   {src: "https://images.tokopedia.net/img/cache/700/VqbcmM/2022/12/6/b9359c63-6a9b-406f-b75f-38e7a9af7c3f.jpg.webp", href: "https://tokopedia.link/40KfGTW0IDb"}
 ];
-  function displayImage() {
-    var randomIndex = Math.floor(Math.random() * images.length);
-    var image = images[randomIndex];
-    var link = document.createElement("a");
-    link.href = image.href;
-    link.target = "_blank";
-    var img = document.createElement("img");
-    img.loading = "lazy";
-    img.src = image.src;
-    img.alt = "Aff";
-    link.appendChild(img);
-    var container = document.getElementById("munculre");
-    container.appendChild(link);
-  }
-  window.onload = displayImage;
